ba612c2b4ad7cd3bd1b8b637ac9c7032179e1d38
Anderson Viana <anderson1565@live.com>
2023-07-02 13:28:20 -0300

    server side fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import ApiResults from "./services/api_provider";
 import SearchBar from "./components/searchbar";
 import { useState } from "react";
 import { CardContend } from "./types/card";
-import { loadPosts } from "./services/api_provider1";
 
 export default function Home() {
   const [busca, setBusca] = useState<String | null>(null);
@@ -27,16 +26,12 @@ export default function Home() {
 
   return (
     <main>
-      <body className="content-center">
-        <div className="place-content-center">
-          <div>
-            <SearchBar getBusca={getBusca} />
-            {(data && <ApiResults data={data} />) || (
-              <h1 className="text-red-500 text-xl">Carregando...</h1>
-            )}
-          </div>
+      <div className="place-content-center">
+        <div>
+          <SearchBar getBusca={getBusca} />
+          {data && <ApiResults data={data} />}
         </div>
-      </body>
+      </div>
     </main>
   );
 }
